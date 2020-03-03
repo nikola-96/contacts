bbb957d3566ef996318c084188ce9fcf48e4b8c3
Nikola <nikolaviki@hotmail.com>
2020-03-03 18:13:00 +0100

    Zadatak 2

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ContactList />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ContactList from "./components/ContactList.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    ContactList
   }
-}
+};
 </script>
 
 <style>
